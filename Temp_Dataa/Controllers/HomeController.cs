a170dbe59d0d6cc5974382b81ab6f68c6ac1a0d6
gaurav2000 <gauravkawde@nimapinfotech.com>
2023-07-24 18:53:25 +0530

    DONE

---
+++ Temp_Dataa/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Temp_Dataa.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["var1"] ="THis is MY VIEW DATA";
+            ViewBag.var2= "THis is MY VIEW Bag";
+            TempData["var3"]= "THis is MY Temp DATA";
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
